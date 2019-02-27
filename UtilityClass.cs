913f6cd5d0914f9d921fc80305139c8f2a1b8ffe
GotohFumiaki <fgotoh@smile-lab.com>
2019-02-27 16:44:15 +0900

    連番作成メソッドの改修

--- UtilityClass.cs
+++ UtilityClass.cs
@@ -6,9 +6,11 @@ public static class UtilityClass
 {
     public static List<int> GenerateSerialNumber(int startNum, int endNum)
     {
-        List<int> serialNumber = new List<int>();
-        IEnumerable<int> serial = Enumerable.Repeat(++startNum, endNum);
-        serialNumber = serial.ToList();
-        return serialNumber;
+        return Enumerable.Repeat(++startNum, endNum).ToList();
+    }
+
+    public static int AdvancedRoundINT(int num, int decimalPointPosition)
+    {
+        return 0;
     }
 }
