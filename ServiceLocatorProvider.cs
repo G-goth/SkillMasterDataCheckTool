6b8cb593d5e268b7c815fea7a6ba735888a9a421
GotohFumiaki <fgotoh@smile-lab.com>
2019-03-14 16:59:40 +0900

    ServiceLocatorProviderクラスへのSingletonパターンの適用

--- ServiceLocatorProvider.cs
+++ ServiceLocatorProvider.cs
@@ -4,8 +4,15 @@ namespace SkillMasterDataCheckTool
 {
     public sealed class ServiceLocatorProvider
     {
+        private static ServiceLocatorProvider _singleton = new ServiceLocatorProvider();
+        public static ServiceLocatorProvider GetInstance()
+        {
+            return _singleton;
+        }
+        private ServiceLocatorProvider()
+        {}
+        
         public ServiceLocator Current { get; private set; }
-
         private void RegisterDependency()
         {
             Current = new ServiceLocator();
