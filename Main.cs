e8d37e4292ed7da7cbf22cdc1eb51e510755321b
GotohFumiaki <fgotoh@smile-lab.com>
2019-04-17 16:50:57 +0900

    ディレクトリにあるExcelファイルを選んでIDを生成できるように

--- Main.cs
+++ Main.cs
@@ -16,34 +16,46 @@ namespace SkillMasterDataCheckTool
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("現在生成中・・・");
-            // UtilityClassで疎結合をやってみるテスト
-            utilityProvider = ServiceLocatorProvider.GetInstance.utilityCurrent.Resolve<IUtilityClassProvider>();
-            utilityProvider.ITestMethod();
-
             // ファイルのフルパスの取得
             List<string> fileFullPath = new List<string>();
             ioFileNamesProvider = ServiceLocatorProvider.GetInstance.ioFileNameCurrent.Resolve<IIOFileNamesProvider>();
             fileFullPath.AddRange(ioFileNamesProvider.GetSpecifiedExtensionFileFullPath("xlsx"));
-            // Console.WriteLine(ioFileNamesProvider.GetSpecifiedExtensionFileNameToList(fileFullPath)[0]);
-            
-            // Excelファイルの最大シート数を取得
-            ioExcelFilesProvider = ServiceLocatorProvider.GetInstance.ioExcelFileCurrent.Resolve<IIOExcelFilesProvider>();
-            int maxSheetNumber = ioExcelFilesProvider.GetExcelSheetNumberMax(fileFullPath[0]);
-            // Excelファイルのシート数を連番で取得
-            List<int> serialNumber = new List<int>(ioExcelFilesProvider.GetExcelSheetNumberList(fileFullPath[0]));            
-
-            // List<XLWorkbook> workBookList = new List<XLWorkbook>();
-            // List<string[][]> XLDataList = new List<string[][]>();
-            // for(int xlFileCount = 1; xlFileCount <= fileFullPath.Count; ++xlFileCount)
-            // {
-            //     XLDataList.Add(ioe.ExtractionExcelData(xlFileCount, ioe.GetExcelObject(fileFullPath[xlFileCount -1])));
-            // }
+            // ファイル名のみを取得
+            List<string> fileNameList = new List<string>();
+            foreach(var name in fileFullPath)
+            {
+                fileNameList.Add(ioFileNamesProvider.GetSpecifiedExtensionFileName(name));
+            }
 
             // ExcelファイルにステージIDとウェーブ数を書き込む
+            ioExcelFilesProvider = ServiceLocatorProvider.GetInstance.ioExcelFileCurrent.Resolve<IIOExcelFilesProvider>();
+            var looping = true;
+            int keyValue = 0;
+            int fileCount = 0;
+            List<int> fileCountList = new List<int>();
+            while(looping)
+            {
+                try
+                {
+                    foreach(var name in fileNameList)
+                    {
+                        fileCountList.Add(++fileCount);
+                        Console.WriteLine(fileCount + " : " + name);
+                    }
+                    Console.WriteLine("いずれかのファイルを数字で選んでください(数字を選んでエンターキーを押す)");
+                    keyValue = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("数字以外が押されました");
+                    continue;
+                }
+            }
+            Console.WriteLine("現在生成中・・・");
             ReadExcelFiles readexcel = new ReadExcelFiles();
-            var sheet = ioExcelFilesProvider.GetExcelObject("test2.xlsx");
-            readexcel.GenerateStageAndWave(sheet, "test2.xlsx");
+            var sheet = ioExcelFilesProvider.GetExcelObject(fileNameList[keyValue - 1]);
+            readexcel.GenerateStageAndWave(sheet, fileNameList[keyValue - 1]);
             Console.ReadKey();
         }
     }
