1def9b9574d6d17684dde7a57896c004dafab470
GotohFumiaki <fgotoh@smile-lab.com>
2019-04-17 11:29:22 +0900

    データの取得をLINQで行う

--- ReadExcelFiles.cs
+++ ReadExcelFiles.cs
@@ -18,40 +18,14 @@ namespace SkillMasterDataCheckTool
             List<string> totalEnemies = new List<string>();
 
             // A列とB列のデータを取得
-            for(int i = 2; i <= row1; ++i)
-            {
-                stageIDs.Add(worksheet1.Cell(i,1).Value.ToString());
-            }
-            for(int i = 2; i <= row1; ++i)
-            {
-                stageIDs.Add(worksheet1.Cell(i,2).Value.ToString());
-            }
-            for(int i = 2; i <= row1; ++i)
-            {
-                totalEnemies.Add(worksheet1.Cell(i,3).Value.ToString());
-            }
+            stageIDs = Enumerable.Range(1, row1).Select(index => worksheet1.Cell(index, 1).Value.ToString()).ToList();
+            stageWave = Enumerable.Range(1, row1).Select(index => worksheet1.Cell(index, 2).Value.ToString()).ToList();
+            totalEnemies = Enumerable.Range(1, row1).Select(index => worksheet1.Cell(index, 3).Value.ToString()).ToList();
 
             // ここにロジック
-            var stageID = Enumerable.Repeat(worksheet1.Cell(2, 1).Value.ToString(), int.Parse(worksheet1.Cell(2, 3).Value.ToString())).ToList();
-            // Console.WriteLine(double.Parse(worksheet1.Cell(2, 3).Value.ToString()) / double.Parse(worksheet1.Cell(2, 2).Value.ToString()));
-            for(int i = 2; i <= int.Parse(worksheet1.Cell(i, 3).Value.ToString()); ++i)
+            foreach(var ids in stageIDs)
             {
-                if(double.Parse(worksheet1.Cell(i, 3).Value.ToString()) / double.Parse(worksheet1.Cell(i, 2).Value.ToString()) >= i)
-                {
-                    for(int i_i = 2; i_i < int.Parse(worksheet1.Cell(i, 3).Value.ToString()) / 2; ++i_i)
-                    {
-                        Console.WriteLine(worksheet1.Cell(i, 1).Value + " : " + 1);
-                    }
-                }
-                else if(int.Parse(worksheet1.Cell(2, 2).Value.ToString()) == 1) return;
-
-                if(double.Parse(worksheet1.Cell(i, 3).Value.ToString()) / double.Parse(worksheet1.Cell(i, 2).Value.ToString()) <= i)
-                {
-                    for(int i_i = i; i_i <= int.Parse(worksheet1.Cell(i, 3).Value.ToString()); ++i_i)
-                    {
-                        Console.WriteLine(worksheet1.Cell(i, 1).Value + " : " + 2);
-                    }
-                }
+                Console.WriteLine(ids);
             }
             Console.WriteLine("Done!!");
         }
